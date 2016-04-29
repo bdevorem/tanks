4f3818a22c93bfd7373da169893ed9f53f502b8c
erunburg <ElliottRunburg@elliotts-mbp.dhcp.nd.edu>
2016-04-29 19:26:12 -0400

    cleaned up main

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class GameSpace(object):
 		self.screen = pygame.display.set_mode(self.size)
 		self.background = pygame.image.load("wood.png")
 		self.back_rect = self.background.get_rect()
-		self.black = (0,0,0)
 
 		#2) Set up game objects
 		self.clock = pygame.time.Clock()
@@ -35,33 +34,13 @@ class GameSpace(object):
 				if event.type == QUIT:
 					sys.exit()
 			self.screen.blit(self.background, self.back_rect)
-			pygame.display.flip()
-"""				elif event.type == KEYDOWN:
-					self.deathstar.move(event.key)
-				elif event.type == MOUSEBUTTONDOWN:
-					self.deathstar.tofire = True
-					self.deathstar.fire_x, self.deathstar.fire_y = pygame.mouse.get_pos()
-				elif event.type == MOUSEBUTTONUP:
-					self.deathstar.tofire = False
 
 			#6) Send ticks to objects
-			self.deathstar.tick()
-			if self.planet.hp <= 0:
-				self.explosion.tick()
-			self.planet.tick()
-			for laser in self.lasers:
-				laser.tick()
-"""
 
 			#7) Display game objects
-"""			for laser in self.lasers:
-				self.screen.blit(laser.image, laser.rect)
-			self.screen.blit(self.deathstar.image, self.deathstar.rect)
-			if self.planet.hp <= 0:
-				self.screen.blit(self.explosion.image, self.explosion.rect)
-			self.screen.blit(self.planet.image, self.planet.rect)
 			pygame.display.flip()
-"""
+
+
 if __name__ == '__main__':
 	gs = GameSpace()
 	gs.start()		
