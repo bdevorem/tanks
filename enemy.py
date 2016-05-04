353b6a468ff8f071630fd12ce6b1778b09149fda
breanna <bdevorem@nd.edu>
2016-05-03 23:26:55 -0400

    add comments to enemy

--- enemy.py
+++ enemy.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python2
+# Breanna Devore-McDonald
+# Elliott Runburg
+# enemy.py
+# class for the random enemies
 import sys
 import os
 import pygame
@@ -11,9 +16,16 @@ from copy import deepcopy
 
 class Enemy(pygame.sprite.Sprite):
 	def __init__(self, gs=None, center=None):
+		"""
+		Enemy class: sort of a hybrid between the user
+		tank and the pellets. It is a tank, and the gun
+		follows the user at all time. But it changes directions
+		when it hits another enemy or a wall
+		"""
 		pygame.sprite.Sprite.__init__(self)
 		self.gs = gs
 
+		# random angle for motion
 		self.angle = random.uniform(1, 360)
 		
 		self.center = center
@@ -29,16 +41,22 @@ class Enemy(pygame.sprite.Sprite):
 		self.exploded = False
 		self.tofire = False
 		self.gun = Gun(self.rect.center, self.gs)
+
+		#random interval at which to fire at user
 		self.fire_interval = random.randint(100, 500)
 		self.fire_timer = 0
 
 	def tick(self):
 		if not self.exploded:
+			# first check if direction needs to be changed
 			self.checkBounce()
+			# then move. will always move, unlike user
 			self.move(self.gs.tank1)
 
 			self.fire_timer += 1
 			if self.fire_timer == self.fire_interval:
+				#create pellets when the interval reaches
+				# the time specified at creation
 				self.fire_timer = 0
 				fire_x, fire_y = self.gs.tank1.rect.center
 				angle = math.atan2(self.rect.centery-fire_y, 
@@ -46,10 +64,14 @@ class Enemy(pygame.sprite.Sprite):
 				pellet_center = (self.rect.centerx+math.cos(angle)*36,
 							self.rect.centery-math.sin(angle)*36)
 				pellet = Pellet(self, angle, pellet_center, self.gs)
-#				self.gs.pellets.append(pellet)
+				self.gs.pellets.append(pellet)
 
 
 	def move(self, tank1):
+		"""
+		Move tank at every tick in the same direction, until	
+		another object is hit or we die
+		"""
 		mx, my = tank1.rect.center
 		dx = mx - self.rect.centerx
 		dy = self.rect.centery - my
@@ -61,20 +83,27 @@ class Enemy(pygame.sprite.Sprite):
 			self.gun.rotate(dx, dy)
 
 	def checkBounce(self):
+		"""
+		A check for if the motion needs to be changed due
+		to a collision (a bounce)
+		"""
 		orig_center = self.rect.center
 		self.temp_rect = self.rect.copy()
 		horiz_coll = False
 		vert_coll = False
 
+		# loop through blocks and compare rectangles
 		self.temp_rect = self.temp_rect.move(self.dx, 0)		
 		for block in self.gs.blocks:
 			if pygame.Rect.colliderect(self.temp_rect, block.rect):
 				horiz_coll = True
+		# same for other enemies besides self
 		for enemy in self.gs.enemies:
 			if enemy is not self:
 				if pygame.Rect.colliderect(self.temp_rect, enemy.rect):
 					horiz_coll = True
 	
+		# repeat for vertical collisions
 		self.temp_rect = self.rect.copy()
 		self.temp_rect = self.rect.move(0, self.dy)
 		for block in self.gs.blocks:
@@ -85,6 +114,7 @@ class Enemy(pygame.sprite.Sprite):
 				if pygame.Rect.colliderect(self.temp_rect, enemy.rect):
 					vert_coll = True
 
+		# change direction if there is 
 		if horiz_coll:
 			self.dx = -1 * self.dx
 		if vert_coll:
@@ -94,6 +124,9 @@ class Enemy(pygame.sprite.Sprite):
 			self.move(self.gs.tank1)
 
 	def explode(self):
+		"""
+		Function to create explosion object when enemy dies
+		"""
 		if not self.exploded:
 			self.gs.enemies.remove(self)
 			self.exploded = True
