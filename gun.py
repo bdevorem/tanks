b79ace68d520506b772b10aeb6a7ae86279d1404
breanna <bdevorem@nd.edu>
2016-05-03 23:13:38 -0400

    add gun comments

--- gun.py
+++ gun.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python2
+# Breanna McDonald
+# Elliott Runburg
+# gun.py
+
 import sys
 import os
 import pygame
@@ -8,6 +12,12 @@ from pellet import Pellet
 
 class Gun(pygame.sprite.Sprite):
 	def __init__(self, center=None, gs=None):
+		"""
+		Not too complicated, load gun images and set
+		rect to that of the tank. The gun image has
+		a transparent backgound and will share a center
+		with the tank to allow for easy rotations
+		"""
 		self.gs = gs
 		self.image = pygame.image.load("imgs/gun.png")
 		self.orig_image = pygame.image.load("imgs/gun.png")
@@ -15,10 +25,19 @@ class Gun(pygame.sprite.Sprite):
 		self.rect.center = center
 
 	def move(self, center):
+		"""
+		Moves depending on the key that gets collected
+		in the tank class
+		"""
 		#print "made it"
 		self.rect = self.rect.move(center)
 
 	def rotate(self, dx, dy):
+		"""
+		Rotate depending on the key collected in tank
+		"""
 		self.image = pygame.transform.rotate(self.orig_image, 
 							math.atan2(dy,dx)/math.pi*180)
 		self.rect = self.image.get_rect(center=self.rect.center)
+
+
